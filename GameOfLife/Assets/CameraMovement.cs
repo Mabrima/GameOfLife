0cade001856ad3de9dbde2dfad3c5904a77f6752
Mabrima <31730695+Mabrima@users.noreply.github.com>
2018-10-25 10:47:12 +0200

    Cameramovement

---
+++ GameOfLife/Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+        float z = Input.GetAxis("Mouse ScrollWheel") * speed * 5;
+
+        
+
+        transform.Translate(x, y, z);
+	}
+}
